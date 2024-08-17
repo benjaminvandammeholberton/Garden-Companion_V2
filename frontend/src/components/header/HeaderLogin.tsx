7f39110eec898c95cf192a41cd801da4e846c6e8
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-08-17 09:03:55 +0200

    start implementing login and register page

---
+++ frontend/src/components/header/HeaderLogin.tsx
@@ -0,0 +1,153 @@
+import { useState } from "react";
+
+import HeaderModal from "../../modal/HeaderModal";
+
+// import burgerMenuIcon from "../../assets/header/burger-menu.png";
+// import chatIcon from "../../assets/header/chat.png";
+import logoutIcon from "../../assets/header/logout.png";
+// import notificationsIcon from "../../assets/header/notification.png";
+// import userIcon from "../../assets/header/user.png";
+// import basketIcon from "../../assets/header/shopping-basket.png";
+import darkModeIcon from "../../assets/header/dark-mode.png";
+import { useNavigate } from "react-router-dom";
+import NavbarMobile from "../navbar/NavbarMobile";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalContent, setModalContent] = useState<string | null>(null);
+  const [isModalNavBarOpen, setIsModalNavBarOpen] = useState<boolean>(false);
+
+  const closeModalNavBar = () => {
+    setIsModalNavBarOpen(false);
+  };
+
+  // const toggleModal = (content: string | null) => {
+  //   if (content === modalContent) {
+  //     setIsModalOpen(false);
+  //     setModalContent(null);
+  //   } else {
+  //     setIsModalOpen(true);
+  //     setModalContent(content);
+  //   }
+  // };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalContent(null);
+  };
+
+  const today = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("JWTGP");
+    navigate("/");
+  };
+  return (
+    <div className="w-full fixed top-0 z-50 bg-white border-b">
+      {/* <div className="lg:hidden flex flex-col items-center w-full gap-2 lg:pb-2">
+        <div className="flex gap-5 items-center"></div>
+        <div className="text-center">
+          <h1 className="text-2xl lg:text-3xl font-thin ">
+            Garden Companion <span className="text-sm lg:text-xl ">Beta</span>
+          </h1>
+          <span className="font-thin lg:text-lg md:block">
+            {today.toLocaleDateString("fr-FR", options)}
+          </span>
+        </div>
+        <ul className="flex gap-10 xl:text-xl">
+          <li className="border-b border-black cursor-pointer">Companion</li>
+          <li className="cursor-pointer">Boutique</li>
+          <li className="cursor-pointer">Blog</li>
+          <li className="cursor-pointer">Forum</li>
+        </ul>
+        <div className="flex items-center w-full justify-center gap-10 border-y py-1">
+          <img
+            className="w-8 h-10 cursor-pointer"
+            src={burgerMenuIcon}
+            alt=""
+            onClick={() => setIsModalNavBarOpen(true)}
+          />
+          <img
+            className="w-8 h-8 cursor-pointer "
+            src={notificationsIcon}
+            alt=""
+          />
+          <img className="w-9 h-9" src={chatIcon} alt="" />
+          <img
+            className="header-modal-button w-9 h-9 cursor-pointer"
+            src={basketIcon}
+            alt=""
+          />
+          <img className="w-9 h-9" src={userIcon} alt="" />
+        </div>
+      </div> */}
+
+      <div className=" bg-white opacity-95 flex items-center justify-center w-full border-b px-10 py-2">
+        {/* <img
+          className="w-8 h-8 cursor-pointer"
+          src={logoutIcon}
+          alt=""
+          onClick={handleLogout}
+        /> */}
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl lg:text-3xl font-thin ">
+            Garden Companion <span className="text-sm lg:text-xl ">Beta</span>
+          </h1>
+          <span className="font-thin lg:text-md">
+            {today.toLocaleDateString("fr-FR", options)}
+          </span>
+        </div>
+        {/* <div>
+          <ul className="flex gap-10 xl:text-xl">
+            <li className="border-b border-black cursor-pointer">Companion</li>
+            <li className="cursor-pointer">Boutique</li>
+            <li className="cursor-pointer">Blog</li>
+            <li className="cursor-pointer">Forum</li>
+          </ul>
+        </div> */}
+        <div className="flex gap-5 items-center">
+          {/* <img
+            className="header-modal-button w-8 h-8 hidden lg:block cursor-pointer"
+            src={notificationsIcon}
+            alt=""
+            onClick={() => toggleModal("notifications")}
+          />
+          <img
+            className="header-modal-button w-9 h-9 cursor-pointer"
+            src={chatIcon}
+            alt=""
+            onClick={() => toggleModal("chat")}
+          />
+          <img
+            className="header-modal-button w-9 h-9 cursor-pointer"
+            src={userIcon}
+            alt=""
+            onClick={() => toggleModal("settings")}
+          />
+          <img
+            className="header-modal-button w-9 h-9 cursor-pointer"
+            src={basketIcon}
+            alt=""
+          />
+
+          <img className="w-8 h-10 lg:hidden" src={burgerMenuIcon} alt="" /> */}
+          {/* <img className="w-8 h-8 cursor-pointer" src={darkModeIcon} alt="" /> */}
+        </div>
+      </div>
+      <HeaderModal
+        isOpen={isModalOpen}
+        content={modalContent}
+        onClose={closeModal}
+      />
+      <NavbarMobile onClose={closeModalNavBar} isOpen={isModalNavBarOpen} />
+    </div>
+  );
+};
+export default Header;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 import { updateTokenInAxiosHeaders } from "../api/axios";
+import HeaderLogin from "../components/header/HeaderLogin";
 
 interface LoginForm {
   email: string;
@@ -52,36 +53,53 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col gap-5 items-center justify-center">
+    <div className="h-screen w-screen flex flex-col gap-5 items-center justify-center main-background ">
+      <HeaderLogin />
+      <div className=" 
+              w-[370px] h-[350px]
+              bg-white
+              opacity-90
+              rounded-3xl
+              flex flex-col
+              justify-between
+              items-center
+              p-3
+              "
+            >
       <h1 className="text-3xl">Login</h1>
       {isLoading && <span>Chargement</span>}
-      <form onSubmit={submitLoginForm}>
-        <div className="flex gap-3">
-          <label htmlFor="email">Email</label>
-          <input
-            className="border"
-            type="email"
-            id="name"
-            name="email"
-            value={loginForm.email}
-            onChange={handleInput}
-          />
+      <form onSubmit={submitLoginForm} className="flex flex-col gap-4">
+        <div className="flex flex-col items-center">
+            <label htmlFor="email">Email</label>
+            <input
+              className="border"
+              type="email"
+              id="name"
+              name="email"
+              value={loginForm.email}
+              onChange={handleInput}
+              />
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              className="border"
+              name="password"
+              type="password"
+              id="password"
+              value={loginForm.password}
+              onChange={handleInput}
+              />
+          </div>
+          <button className="border p-2 rounded-3xl mt-2" type="submit">
+            Se connecter
+          </button>
+        </form>
+        <div className="font-thin flex flex-col text-center">
+        <a href="#">Mot de passe oublié</a>
+        <a href="/register">Pas encore inscrit ?</a>
         </div>
-        <div className="flex gap-3">
-          <label htmlFor="password">Password</label>
-          <input
-            className="border"
-            name="password"
-            type="password"
-            id="password"
-            value={loginForm.password}
-            onChange={handleInput}
-          />
-        </div>
-        <button className="border p-3 rounded-3xl" type="submit">
-          Se connecter
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
